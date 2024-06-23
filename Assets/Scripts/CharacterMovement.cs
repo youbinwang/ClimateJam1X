3e2ba49470a9cd46dc136aad4515d9900c04a406
MuMu3an <wangyoubin1205@gmail.com>
2024-06-24 00:04:27 +0800

    Player Input Update

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,32 +8,49 @@ public class CharacterMovement : MonoBehaviour
     [Header("Character Movement")]
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+    private float moveX = 0f;
+    private bool jump = false;
     
     [Header("Ground Check")]
-    public float groundCheckDistance = 0.1f;
+    public float groundCheckDistance = 1f;
     [SerializeField] private LayerMask groundMask;
 
     private Rigidbody rb;
     private bool isGrounded;
-
+    
+    
     void Start()
     {
         rb = GetComponentInChildren<Rigidbody>();
     }
-
+    
     void Update()
-    {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
-        
-        float moveX = Input.GetAxis("Horizontal");
-        
-        Vector3 velocity = rb.velocity;
-        velocity.x = moveX * moveSpeed;
-        rb.velocity = velocity;
+    { 
+        // Horizontal Input
+        moveX = Input.GetAxis("Horizontal");
         
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jump = true;
         }
+    }
+
+    void FixedUpdate()
+    {
+        // Ground Check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
+        
+        // Left and Right Movement
+        Vector3 velocity = rb.velocity;
+        velocity.x = moveX * moveSpeed;
+        rb.velocity = velocity;
+
+        // Jump
+        if (jump && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jump = false;
+        }
+        
     }
 }
