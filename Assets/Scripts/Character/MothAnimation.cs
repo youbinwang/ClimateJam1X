ea486d9272f27095738b67ba5fed85ae34e1c988
astrvea <mrcchen915@gmail.com>
2024-07-12 22:43:38 -0400

    moth animations complete

--- Assets/Scripts/Character/MothAnimation.cs
+++ Assets/Scripts/Character/MothAnimation.cs
@@ -1,17 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class MothAnimation : MonoBehaviour
 {
     private Animator animator;
+    private int currentAnimationIndex = 1; // Start with IdleAnimation1
+    private const float switchInterval = 5f; // Interval between switches in seconds
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        if (animator != null)
+        StartCoroutine(SwitchIdleAnimations());
+    }
+
+    IEnumerator SwitchIdleAnimations()
+    {
+        while (true)
         {
-            animator.SetTrigger("IdleTrigger"); 
+            // Toggle between IdleAnimation1 and IdleAnimation2 based on currentAnimationIndex
+            if (currentAnimationIndex == 1)
+            {
+                animator.SetInteger("IdleSwitchCounter", 1); // Set parameter for IdleAnimation1
+                Debug.Log("Switching to IdleAnimation1");
+                currentAnimationIndex++; // Switch to IdleAnimation2 next
+            }
+            else if (currentAnimationIndex == 2)
+            {
+                animator.SetInteger("IdleSwitchCounter", 2); // Set parameter for IdleAnimation2
+                Debug.Log("Switching to IdleAnimation2");
+                currentAnimationIndex--; // Switch to IdleAnimation1 next
+            }
+
+            Debug.Log("Current Animation Index: " + currentAnimationIndex);
+            Debug.Log("IdleSwitchCounter: " + animator.GetInteger("IdleSwitchCounter"));
+
+            yield return new WaitForSeconds(switchInterval);
         }
     }
 }
