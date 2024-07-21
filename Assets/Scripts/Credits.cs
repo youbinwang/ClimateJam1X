c98ab9ae402ca504c5d31fbda35b4c337a919744
toastyghostii <kikithecoolcookie@gmail.com>
2024-07-20 18:51:34 -0700

    Credits Added

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private Animator Fade;
+    [SerializeField] private Animator rollCredits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fade.Play("fade-from-black");
+        //rollCredits.Play("credits", 0, 4f);
+        //Fade.Play("fade-to-black", 0, 120f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
