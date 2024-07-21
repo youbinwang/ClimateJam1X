79190fb3fdeceb8fef805753d54254fb71cdadd1
toastyghostii <kikithecoolcookie@gmail.com>
2024-07-20 19:25:04 -0700

    Credits Completed

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    [SerializeField] private Animator Fade;
-    [SerializeField] private Animator rollCredits;
+    [SerializeField] private GameObject blackScreen;
+    private Animator fadeAnims;
+
+    [SerializeField] private Animator credits;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Fade.Play("fade-from-black");
-        //rollCredits.Play("credits", 0, 4f);
-        //Fade.Play("fade-to-black", 0, 120f);
+        fadeAnims = blackScreen.GetComponent<Animator>();
+        fadeAnims.Play("fade-from-black");
+
+        Invoke("fadeGone", 3.9f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void fadeGone()
+    {
+        blackScreen.SetActive(false);
+
+        Invoke("rollCredits", 1f);
+    }
+
+    private void rollCredits()
+    {
+        credits.Play("credits");
+
+        Invoke("fadeOut", 115f);
+    }
+
+    private void fadeOut()
+    {
+        blackScreen.SetActive(true);
+        fadeAnims.Play("fade-to-black");
+
+        Invoke("mainMenu", 4f);
+    }
+
+    private void mainMenu()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
